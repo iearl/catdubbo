94d5d5891a189b4c014abf958bccc73f2ed4585b
iearl <dc_magaowei@163.com>
2019-10-26 22:10:23 +0800

    用户模块添加实体类及相关接口方法

--- guns/guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
+++ guns/guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -2,5 +2,14 @@ package com.stylefeng.guns.api.user;
 
 
 public interface UserAPI {
+    //登录
     boolean login(String username, String password);
+    //注册
+    boolean register(UserModel userModel);
+    //检查用户名是否存在
+    boolean checkUsername(String username);
+    //查询用户信息
+    UserInfoModel getUserInfo(int uuid);
+    //更新用户信息
+    UserInfoModel updateUserInfo(UserInfoModel userInfoModel);
 }
