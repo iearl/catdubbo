5d7f98cfd3b6e75f9e7ddcfe6446bcf2f56a2ad9
iearl <dc_magaowei@163.com>
2019-10-28 12:46:57 +0800

    jwt修改规则验证

--- guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/example/ExampleController.java
+++ guns/guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/example/ExampleController.java
@@ -1,5 +1,6 @@
 package com.stylefeng.guns.rest.modular.example;
 
+import com.stylefeng.guns.rest.common.CurrentUser;
 import com.stylefeng.guns.rest.common.SimpleObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,12 @@ public class ExampleController {
     @RequestMapping("")
     public ResponseEntity hello(@RequestBody SimpleObject simpleObject) {
         System.out.println(simpleObject.getUser());
+        return ResponseEntity.ok("请求成功!");
+    }
+    @RequestMapping("threadLocal")
+    public ResponseEntity testThreadLocal() {
+        System.out.println("userId="+CurrentUser.getUserId());
+        //userId存在redis中，也可查询数据库
         return ResponseEntity.ok("请求成功!");
     }
 }
